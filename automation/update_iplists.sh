b30ef2001831bc85e9457d79cb7d1281b14055b3
Davie3 <Davie3@users.noreply.github.com>
2022-06-21 12:41:00 -0700

    Add check to not commit the generated lists if a CloudFlare error page is detected.

--- automation/update_iplists.sh
+++ automation/update_iplists.sh
@@ -5,7 +5,9 @@ NOW=$(date +"%m-%d-%Y %H:%M:%S PT")
 
 /usr/bin/python3 cloudflare_ips.py
 
-/usr/bin/git add ../cloudflare-ips-v*
-/usr/bin/git status
-/usr/bin/git commit --author="Davie3 <Davie3@users.noreply.github.com>" -m "Run automations and generate fresh lists - $NOW"
-/usr/bin/git push
+if ! grep -q "error" "../cloudflare-ips-v4.rsc" && ! grep -q "error" "../cloudflare-ips-v6.rsc"; then
+    /usr/bin/git add ../cloudflare-ips-v*
+    /usr/bin/git status
+    /usr/bin/git commit --author="Davie3 <Davie3@users.noreply.github.com>" -m "Run automations and generate fresh lists - $NOW"
+    /usr/bin/git push
+fi
